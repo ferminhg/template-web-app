51528044528845cb63ce671f43ab840e34515483
Fermin <josefermin.hernandez@adevinta.com>
2022-12-24 13:10:36 +0100

    feat: create simple componets:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,75 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from "../src/components/Header";
+import Footer from "../src/components/Footer";
+import WIPContent from "../src/components/WIPContent";
 
 export default function Home() {
     return (
         <>
-            <Head>
-                <title>Template web-app</title>
-                <meta name="description" content="Template web-app" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className={styles.main}>
-                <div role="heading" className={styles.description}>
-                    <p>
-                        🚧 Web-app Template&nbsp;
-                        <code className={styles.code}>pages/index.tsx</code>
-                    </p>
-                </div>
-
-                <div className={styles.center}>
-                    <Image
-                        alt="This is fine"
-                        src="/this-is-fine.webp"
-                        width={480}
-                        height={270}
-                        />
-                </div>
-
-                <div className={styles.grid}>
-                    <div className={styles.card}>
-                        <h2 className={inter.className}>
-                            Menu <span>-&gt;</span>
-                        </h2>
-                        <p className={inter.className}>
-                            Find in-depth information about Next.js features and&nbsp;API.
-                        </p>
-                    </div>
-
-                    <div className={styles.card}>
-                        <h2 className={inter.className}>
-                            Menu <span>-&gt;</span>
-                        </h2>
-                        <p className={inter.className}>
-                            Learn about Next.js in an interactive course with&nbsp;quizzes!
-                        </p>
-                    </div>
-
-                    <div className={styles.card}>
-                        <h2 className={inter.className}>
-                            Menu <span>-&gt;</span>
-                        </h2>
-                        <p className={inter.className}>
-                            Discover and deploy boilerplate example Next.js&nbsp;projects.
-                        </p>
-                    </div>
-
-                    <div className={styles.card}>
-                        <h2 className={inter.className}>
-                            Menu <span>-&gt;</span>
-                        </h2>
-                        <p className={inter.className}>
-                            Instantly deploy your Next.js site to a shareable URL
-                            with&nbsp;Vercel.
-                        </p>
-                    </div>
-                </div>
-            </main>
+            <Header/>
+            <WIPContent/>
+            <Footer/>
         </>
     )
 }
